3b43380ecda8a54e1cece39c54d27f3615f43c3f
Idoufkir <mustafa.idoufkir@gmail.com>
2020-11-09 23:49:13 +0100

    Update veiw

--- routes/pages.js
+++ routes/pages.js
@@ -36,7 +36,7 @@ router.get('/add-mgsn', authGuard.isAuth, (req, res) => {
   });
   });
 
-router.post('/save-mag', (req, res) => {
+router.post('/save-mag', authGuard.isAuth, (req, res) => {
     let data = {nom: req.body.nom, prenom: req.body.prenom, salaire: req.body.salaire, age: req.body.age, tele: req.body.tele, id_spct: req.body.id_spct};
     let sql = "INSERT INTO magasinier SET ?";
     connection.query(sql, data,(err, results) => {
@@ -44,6 +44,30 @@ router.post('/save-mag', (req, res) => {
       res.redirect('/dashboard');
     });
 });
+
+router.get('/delete/mag/:idM',(req, res) => {
+  const idM = req.params.idM;
+  let sql = `DELETE from magasinier where id_mgsn = ${idM}`;
+  let query = connection.query(sql,(err, result) => {
+      if(err) throw err;
+      res.redirect('/dashboard');
+  });
+});
+
+
+router.get('/edit/mag/:idM',(req, res) => {
+
+  const idM = req.params.idM;
+  let sql = `SELECT * FROM  (magasinier FULL JOIN specialite) where id_mgsn = ${idM}`;
+  let query = connection.query(sql,(err, result) => {
+      if(err) throw err;
+      res.render('modifier-mag', {
+          title : 'Modification des données Mag',
+          row : result[0]
+      });
+  });
+});
+
 
 
 module.exports = router;
